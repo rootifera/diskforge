74196c911580b5fe425850be88422c876faca240
Omur Ozbahceliler <omur@rootifera.org>
2024-05-10 14:52:16 +0100

    clear partitions added

--- functions.py
+++ functions.py
@@ -1,6 +1,14 @@
+import logging
 import os
 import subprocess
 import sys
+import threading
+import time
+
+from tqdm import tqdm
+
+# Set up logging
+logging.basicConfig(filename='diskforge.log', level=logging.INFO)
 
 
 def _all_disks():
@@ -36,7 +44,7 @@ def identify_disks():
         print("Error: Unable to identify the OS disk. Operation halted.")
         sys.exit(1)
 
-    # remove the OS disk
+    # remove the OS disk from the disks list
     disk_list.remove(os_disk)
 
     if len(disk_list) == 0:
@@ -45,3 +53,50 @@ def identify_disks():
 
     return disk_list
 
+
+def clear_partitions(disk, progress_bar, success_count, failure_count):
+    try:
+        # Using parted for this operation
+        subprocess.run(['sudo', 'parted', '--script', disk, 'mklabel', 'gpt'], check=True)
+        progress_bar.update(1)
+        success_count.append(disk)
+        # logs goes into diskforge.log
+        logging.info(f"Partitions cleared for disk {disk}")
+    except subprocess.CalledProcessError as e:
+        failure_count.append(disk)
+        logging.error(f"Failed to clear partitions for disk {disk}: {e}")
+
+
+def clear_partitions_all(disks):
+    success_count = []
+    failure_count = []
+
+    print(f"Total Disks found: {len(disks)}")
+    print("Clearing partition tables...")
+
+    # adding '/dev/' to disk names
+    dev_disks = ['/dev/' + disk for disk in disks]
+
+    # single progress bar for all
+    progress_bar = tqdm(total=len(dev_disks), desc="Overall Progress")
+
+    # creating threads for each disk
+    threads = []
+    for dev_disk in dev_disks:
+        thread = threading.Thread(target=clear_partitions, args=(dev_disk, progress_bar, success_count, failure_count))
+        threads.append(thread)
+        thread.start()
+
+    # waiting for all to complete
+    for thread in threads:
+        thread.join()
+
+    progress_bar.close()
+    # this is here to make the progress bar visible, otherwise it just disappears
+    time.sleep(2)
+
+    # reset the terminal, just a lazy trick to print properly
+    os.system('reset')
+
+    print(f"Total Success: {len(success_count):<5}")
+    print(f"Total Failure: {len(failure_count):<5}")
