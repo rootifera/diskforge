9417ef703c0187bec04b8fcf46e121582d69ca3e
Omur Ozbahceliler <omur@rootifera.org>
2024-06-04 13:49:40 +0100

    ctrl+c handler added

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import signal
+import sys
+
 import diskforge
 from colorama import Fore, init
 
 init(autoreset=True)
 
+
+def signal_handler(sig, frame):
+    print('\nExiting...')
+    sys.exit(0)
+
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     disks = diskforge.identify_disks()
     print(f"{Fore.BLUE}====================================")
     diskforge.check_disk_health(disks)
