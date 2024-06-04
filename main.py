ebf14b33e7163fa49ad3f34aca763124b815ebe3
Omur Ozbahceliler <omur@rootifera.org>
2024-06-04 13:57:01 +0100

    formatting

--- main.py
+++ main.py
@@ -14,22 +14,19 @@ def signal_handler(sig, frame):
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
+
+    print(f"{Fore.BLUE}=========== OS Disks ===============")
     disks = diskforge.identify_disks()
-    print(f"{Fore.BLUE}====================================")
+    print(f"{Fore.BLUE}=========== Disk Health ============")
     diskforge.check_disk_health(disks)
-    print(f"{Fore.BLUE}====================================")
-
+    print(f"{Fore.BLUE}=========== Disk Size ==============")
     diskforge.visualize_disk_sizes(disks)
-    print(f"{Fore.BLUE}====================================")
-
+    print(f"{Fore.BLUE}=========== Confirmation ===========")
     diskforge.confirm_action(disks)
     print(f"{Fore.BLUE}====================================")
-
     diskforge.clear_partitions_all(disks)
     print(f"{Fore.BLUE}====================================")
-
     diskforge.format_all_disks(disks)
     print(f"{Fore.BLUE}====================================")
-
     diskforge.set_labels(disks)
     print(f"{Fore.BLUE}====================================")
